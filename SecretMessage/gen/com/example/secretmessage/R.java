b9cb257fa4dd6b6d9dc78c945d041691c615db6d
Daniel Hilton <hilton.daniel0@gmail.com>
2014-03-23 12:19:06 +0100

    Creating login screen

--- SecretMessage/gen/com/example/secretmessage/R.java
+++ SecretMessage/gen/com/example/secretmessage/R.java
@@ -24,10 +24,28 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080010;
+        public static final int button_LoginTest=0x7f08000e;
+        public static final int button_changepass=0x7f080006;
+        public static final int image_LoginLogo=0x7f08000f;
+        public static final int password_bar_bottom=0x7f080009;
+        public static final int password_bar_top=0x7f080007;
+        public static final int password_rule_bottom=0x7f08000a;
+        public static final int password_rule_top=0x7f080008;
+        public static final int password_wrapper=0x7f08000c;
+        public static final int relativeLayout_changepass=0x7f080000;
+        public static final int textView_changepass_prompt=0x7f080002;
+        public static final int textView_changepass_title=0x7f080001;
+        public static final int textView_changpass_reprompt=0x7f080004;
+        public static final int text_LoginPassword=0x7f08000d;
+        public static final int text_LoginPasswordPrompt=0x7f08000b;
+        public static final int text_changepass=0x7f080003;
+        public static final int text_changepass_again=0x7f080005;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_changepassword=0x7f030000;
+        public static final int activity_login=0x7f030001;
+        public static final int activity_main=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -35,7 +53,27 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int button_Authenticate=0x7f050010;
+        public static final int button_ChangePassword=0x7f05000e;
+        public static final int button_SaveSettngs=0x7f050011;
+        public static final int button_clear_history=0x7f050008;
+        public static final int button_send_text=0x7f050004;
+        public static final int changepass_prompt=0x7f050013;
+        public static final int changepass_reprompt=0x7f050014;
+        public static final int changepass_title=0x7f050012;
+        public static final int clear_history_title=0x7f05000d;
+        public static final int history_hint=0x7f050005;
+        public static final int login_password_title=0x7f05000f;
+        public static final int message_box_hint=0x7f050002;
+        public static final int method_openSettings=0x7f050006;
+        public static final int newmessage_prompt=0x7f050016;
+        public static final int newmessage_title=0x7f050015;
+        public static final int phone_number_hint=0x7f050003;
+        public static final int radio_authtype_title=0x7f05000c;
+        public static final int radio_none_label=0x7f05000b;
+        public static final int radio_pass_label=0x7f05000a;
+        public static final int radio_pin_label=0x7f050009;
+        public static final int settings_title=0x7f050007;
     }
     public static final class style {
         /** 
